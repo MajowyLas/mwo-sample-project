00d2fb42d093599b53f689faf20ec4becb0853a6
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-05-31 10:50:09 +0200

    PitStop.java

--- mwo-sample-project/src/main/java/model/PitStop.java
+++ mwo-sample-project/src/main/java/model/PitStop.java
@@ -1,4 +1,13 @@
 package model;
 
+import java.sql.Date;
+
 public class PitStop {
+
+    int session_key;
+    int meeting_key;
+    Date date;
+    int driver_number;
+    float pit_duration;
+    int lap_number;
 }
