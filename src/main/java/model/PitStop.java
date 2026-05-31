dcd02e122b2ea96ef69d1be363774d7360046842
MajowyLas <aniamaywald@gmail.com>
2026-05-31 11:52:41 +0200

    Changed "Date date;" na String date -> due to Gson's requirements

--- src/main/java/model/PitStop.java
+++ src/main/java/model/PitStop.java
@@ -1,12 +1,12 @@
 package model;
 
-import java.sql.Date;
+//import java.sql.Date;
 
 public class PitStop {
 
     int session_key;
     int meeting_key;
-    Date date;
+    String date;
     int driver_number;
     float pit_duration;
     int lap_number;
